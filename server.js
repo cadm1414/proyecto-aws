a9053dfaa29443ce295ec0735cd85c0200cf2d55
cadm1414 <cdiazm14@gmail.com>
2025-10-28 14:20:30 -0500

    fix: actualizacion de deploy en aws sin swagger

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('./src/docs/swagger.json');
+//const swaggerUi = require('swagger-ui-express');
+//const swaggerDoc = require('./src/docs/swagger.json');
 const routes = require('./src/routes');
 
 const app = express();
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
 app.use('/', routes);
