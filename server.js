797b8c5f863ce80a2d5d032a77b69b485ecfef3a
cadm1414 <cdiazm14@gmail.com>
2025-10-28 22:01:24 -0500

    fix: actualizacion de deploy sin cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 //const swaggerUi = require('swagger-ui-express');
 //const swaggerDoc = require('./src/docs/swagger.json');
 const routes = require('./src/routes');
@@ -7,7 +7,7 @@ const routes = require('./src/routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
