bedf148785d5d6d38d1fc56dc93c185ca2755b35
cadm1414 <cdiazm14@gmail.com>
2025-11-02 11:21:55 -0500

    fix: terraform sin ip elasticas

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -48,30 +48,30 @@ resource "aws_internet_gateway" "main" {
 }
 
 # Elastic IP para NAT Gateway
-resource "aws_eip" "nat" {
-  count  = 2
-  domain = "vpc"
+#resource "aws_eip" "nat" {
+#  count  = 2
+#  domain = "vpc"
 
-  tags = {
-    Name = "${var.project_name}-${var.environment}-eip-${count.index + 1}"
-  }
+#  tags = {
+#    Name = "${var.project_name}-${var.environment}-eip-${count.index + 1}"
+#  }
 
-  depends_on = [aws_internet_gateway.main]
-}
+#  depends_on = [aws_internet_gateway.main]
+#}
 
 # NAT Gateway
-resource "aws_nat_gateway" "main" {
-  count = 2
+#resource "aws_nat_gateway" "main" {
+#  count = 2
 
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.public[count.index].id
+#  allocation_id = aws_eip.nat[count.index].id
+#  subnet_id     = aws_subnet.public[count.index].id
 
-  tags = {
-    Name = "${var.project_name}-${var.environment}-nat-${count.index + 1}"
-  }
+#  tags = {
+#    Name = "${var.project_name}-${var.environment}-nat-${count.index + 1}"
+#  }
 
-  depends_on = [aws_internet_gateway.main]
-}
+#  depends_on = [aws_internet_gateway.main]
+#}
 
 # Route Table pública
 resource "aws_route_table" "public" {
@@ -88,20 +88,20 @@ resource "aws_route_table" "public" {
 }
 
 # Route Table privada
-resource "aws_route_table" "private" {
-  count = 2
+#resource "aws_route_table" "private" {
+#  count = 2
 
-  vpc_id = aws_vpc.main.id
+#  vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main[count.index].id
-  }
+#  route {
+#    cidr_block     = "0.0.0.0/0"
+#    nat_gateway_id = aws_nat_gateway.main[count.index].id
+#  }
 
-  tags = {
-    Name = "${var.project_name}-${var.environment}-private-rt-${count.index + 1}"
-  }
-}
+#  tags = {
+#    Name = "${var.project_name}-${var.environment}-private-rt-${count.index + 1}"
+#  }
+#}
 
 # Asociaciones de Route Tables públicas
 resource "aws_route_table_association" "public" {
@@ -112,12 +112,12 @@ resource "aws_route_table_association" "public" {
 }
 
 # Asociaciones de Route Tables privadas
-resource "aws_route_table_association" "private" {
-  count = 2
+#resource "aws_route_table_association" "private" {
+ # count = 2
 
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
-}
+  #subnet_id      = aws_subnet.private[count.index].id
+  #route_table_id = aws_route_table.private[count.index].id
+#}
 
 # Data source para availability zones
 data "aws_availability_zones" "available" {
