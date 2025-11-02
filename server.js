1ecdaea841b9369aa990e832c0dd5dbe2c481404
cadm1414 <cdiazm14@gmail.com>
2025-11-01 21:06:53 -0500

    fix: activando swagger

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 //const cors = require('cors');
-//const swaggerUi = require('swagger-ui-express');
-//const swaggerDoc = require('./src/docs/swagger.json');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./src/docs/swagger.json');
 const routes = require('./src/routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 //app.use(cors());
+/* cors for all routes */
 app.use(express.json());
 
-//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-app.get('/health', (req, res) => res.json({ status: 'ok' }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+//app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
 app.use('/', routes);
 app.listen(port, () => console.log(`✅ Server listening on port V2.3 ${port}`));
