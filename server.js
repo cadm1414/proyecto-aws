465f0dcbfab98a919d3c8aa05f932a6abcce05cb
cadm1414 <cdiazm14@gmail.com>
2025-11-01 21:01:05 -0500

    fix: desactivando swagger y cors para prueba de despliegue

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./src/routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
